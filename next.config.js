81bd6cb339345dbc85aa5994dd006dd584506758
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-15 09:45:44 +0000

    temp-log

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 console.log('API_URL_FIRST: ' + process.env.NEXT_TODO_API_URL)
 const getApiPrefix = () => {
   if (typeof process.env.NEXT_TODO_API_URL === 'undefined') {
-    console.log('API_URL_IF: ' + process.env.NEXT_TODO_API_URL)
+    console.log('API_URL_IF_default: ' + process.env.NEXT_TODO_API_URL)
     //設定がなければ自身のAPI
     return 'http://localhost:3000'
   } else {
