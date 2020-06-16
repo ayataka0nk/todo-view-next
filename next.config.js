b49908cf0c85f077aa164c92cafcce324e0c00a4
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-16 07:08:08 +0000

    タスク追加フォームを作成

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
-    apiOrigin: process.env.API_ORIGIN || 'http://localhost:3000',
+    apiOrigin: process.env.API_ORIGIN,
+    publicApiOrigin: process.env.NEXT_PUBLIC_API_ORIGIN,
   },
 }
