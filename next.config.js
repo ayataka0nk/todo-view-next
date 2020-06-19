43c27b443c62237528377a4866e841c479b204f5
naoto kanba <naoto.kanba@dcom-web.co.jp>
2020-06-19 03:59:43 +0000

    接続先ポート変更

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const publicApiOrigin = () => {
   if (process.env.NODE_ENV === 'production') {
     return 'http://118.27.0.46:8000'
   } else if (process.env.NODE_ENV === 'staging') {
-    return 'http://127.0.0.1:18090'
+    return 'http://127.0.0.1:8090'
   } else {
     return 'http://127.0.0.1:18080'
   }
