92761f8219d901f20e4f44956d68a5a30115022e
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-22 06:17:51 +0000

    共通テキストボックスのpropsにonBlurを追加

--- components/Textbox.tsx
+++ components/Textbox.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-type TextboxProps = {
+export type TextboxProps = {
   name: string
   value: string
   onChange: (name: string, value: string) => void
+  onBlur?: (event: React.FocusEvent<HTMLElement>) => void
   readOnly?: boolean
 }
 
 export const Textbox = (props: TextboxProps): JSX.Element => {
-  const { name, value, onChange, readOnly = false } = props
+  const { name, value, onChange, readOnly = false, onBlur = undefined } = props
   const onLocalChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (typeof onChange === 'function') {
       onChange(event.target.name, event.target.value)
@@ -20,6 +21,7 @@ export const Textbox = (props: TextboxProps): JSX.Element => {
         name={name}
         value={value}
         onChange={onLocalChange}
+        onBlur={onBlur}
         readOnly={readOnly}
       />
     </>
