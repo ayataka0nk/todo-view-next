b6ad67238c4a4c8f796c34d1a2970e8ceafc841f
naoto kanba <naoto.kanba@dcom-web.co.jp>
2020-06-22 08:04:43 +0000

    デバッグ用console.logを削除

--- libs/ApiPathResolver.ts
+++ libs/ApiPathResolver.ts
@@ -3,8 +3,6 @@ import getConfig from 'next/config'
 export const resolveApiPath = (apiPathname: string): string => {
   const { publicRuntimeConfig } = getConfig()
   const origin = publicRuntimeConfig.publicApiOrigin
-  console.log('origin: ' + origin + ', apiPath: ' + apiPathname)
   const path = new URL(apiPathname, origin).toString()
-  console.log('resolved path: ' + path)
   return path
 }
